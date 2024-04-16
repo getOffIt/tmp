110496c436052841f5c03e83718444783e176c1f
none <asdf@fasd.com>
2024-04-16 11:58:50 +0100

    asdf

---
+++ xcodecloudtmp/xcodecloudtmpApp.swift
@@ -0,0 +1,16 @@
+//
+//  xcodecloudtmpApp.swift
+//  xcodecloudtmp
+//
+//
+
+import SwiftUI
+
+@main
+struct xcodecloudtmpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
